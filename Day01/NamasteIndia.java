a10997bc15fd611dc8bbc3acb4b04a93ac885901
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2023-05-24 01:12:33 +0530

    Added sorce code file of Day-01

---
+++ Day01/NamasteIndia.java
@@ -0,0 +1,13 @@
+package Java.Day01;
+
+public class NamasteIndia
+{
+
+     public static void main(String[] args)
+     {
+        
+         System.out.println("Namaste India");
+
+     } 
+
+}
---
+++ Day01/P01.java
@@ -0,0 +1,30 @@
+
+package Java.Day01;
+import java.util.Scanner;
+
+public class P01  
+{
+    
+public static void main(String[] args) 
+{
+Scanner scanner = new Scanner(System.in);
+int a , b;
+float sum = 0F;
+
+System.out.println("Enter first number :-- ");
+System.out.println();
+
+a = scanner.nextInt();
+
+System.out.println("Enter second number :-- ");
+System.out.println();
+
+b = scanner.nextInt();
+
+sum = a + b;
+
+System.out.println("The sum is : " + sum);
+
+}
+
+}
